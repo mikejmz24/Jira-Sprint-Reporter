a9558b2efb4f5453d86eb96cd627567e40cc15ce
mikejmnz24 <4935341+mikejmz24@users.noreply.github.com>
2024-04-11 19:59:38 -0700

    Corrected test-entities.py file assert the instance type

--- poetry-demo/tests/test_entities.py
+++ poetry-demo/tests/test_entities.py
@@ -3,9 +3,13 @@ import json
 from entities import jira_issue
 
 
-def test_jira_issue_returns_json_data_when_json_is_provided() -> None:
+def test_jira_issue_from_dict_returns_jira_issue_object_type() -> None:
+    """jira_issue_from_dict method returns a valid JiraIssue object type
+    when a valid JSON is passed as a parameter"""
     with open("../tests/json_files/intgpt-109.json", encoding="utf-8") as json_file:
         data = json.load(json_file)
-        json_data = jira_issue.jira_issue_from_dict(data)
-        print(json_data)
-        assert json_data is not None
+        json_data: jira_issue.JiraIssue = jira_issue.jira_issue_from_dict(data)
+        print("str() string: ", str(json_data))
+
+        print("repr() string: ", repr(json_data))
+        assert isinstance(json_data, jira_issue.JiraIssue)
