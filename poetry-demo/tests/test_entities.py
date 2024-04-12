d214f58abe0c0c04d05707b819e7729ea4fecf5a
mikejmnz24 <4935341+mikejmz24@users.noreply.github.com>
2024-04-12 13:33:16 -0700

    Added neovim dependency to project

--- poetry-demo/tests/test_entities.py
+++ poetry-demo/tests/test_entities.py
@@ -6,7 +6,7 @@ from entities import jira_issue
 def test_jira_issue_from_dict_returns_jira_issue_object_type() -> None:
     """jira_issue_from_dict method returns a valid JiraIssue object type
     when a valid JSON is passed as a parameter"""
-    with open("../tests/json_files/intgpt-109.json", encoding="utf-8") as json_file:
+    with open("json_files/intgpt-109.json", encoding="utf-8") as json_file:
         data = json.load(json_file)
         json_data: jira_issue.JiraIssue = jira_issue.jira_issue_from_dict(data)
         print("str() string: ", str(json_data))
