fe94c63c91857f2b66a58e77123367ba6c1a0027
mikejmnz24 <4935341+mikejmz24@users.noreply.github.com>
2024-04-17 12:01:59 -0700

    Added only the files in the main folder

---
+++ Jira-Sprint-Reporter/entities/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["jira_issue"]
---
+++ Jira-Sprint-Reporter/tests/test_entities.py
@@ -0,0 +1,27 @@
+import json
+
+from entities import jira_issue
+
+
+def test_jira_issue_from_dict_returns_jira_issue_object_type() -> None:
+    """jira_issue_from_dict method returns a valid JiraIssue object type
+    when a valid JSON is passed as a parameter"""
+    with open("json_files/intgpt-109.json", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        json_data: jira_issue.JiraIssue = jira_issue.jira_issue_from_dict(data)
+        print("str() string: ", str(json_data))
+
+        print("repr() string: ", repr(json_data))
+        assert isinstance(json_data, jira_issue.JiraIssue)
+
+
+def test_jira_issue_to_dict_returns_json_object_type() -> None:
+    """jira_issue_to_dict method return a valid Json object type
+    when a valid JiraIssue object is passed as parameter"""
+    with open("json_files/intgpt-109.json", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        print(type(data))
+        json_data: jira_issue.JiraIssue = jira_issue.jira_issue_from_dict(data)
+        json_response = jira_issue.jira_issue_to_dict(json_data)
+        print(json_response)
+        assert isinstance(json_response, dict)
