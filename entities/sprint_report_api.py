090d3a8e8bec9c0a1bcd97507c9b526c974678f6
mikejmnz24 <4935341+mikejmz24@users.noreply.github.com>
2024-04-18 18:38:59 -0700

    Added DocString and JiraIssueSprintReport type

--- entities/sprint_report_api.py
+++ entities/sprint_report_api.py
@@ -1,27 +1,196 @@
+"""
+SprintReport
+____________
+An object that captures the most relevant data of the Jira sprint report rest api.
+Notes
+_____
+https://jira.amer.thermo.com/rest/greenhopper/latest/rapid/charts/sprintreport?rapidViewId={}&sprintId={}
+"""
+
 from dataclasses import dataclass
-from typing import Any
+from datetime import datetime
+from typing import Any, Optional
 
 from utilities import utils
 
 
 @dataclass
-class SprintReport:
-    sprint_id: int
+class JiraIssueSprintReport:
+    """ "
+    The JiraIssueSpritnReport object captures the most relevant data of issues
+    inside the Jira sprint report rest api
+
+    ...
+    Attributes
+    __________
+    key: str
+        The issue key given by Jira.
+
+    """
+
+    jira_issue_id: int
+    key: str
+    summary: str
+    original_estimate: int
+    final_estimate: Optional[int] = None
 
     @staticmethod
-    def from_dict(obj: Any):
-        sprint_id: int = int(utils.get_object_str(obj, "sprint.id"))
-        return SprintReport(sprint_id)
+    def from_dict(obj: Any) -> "JiraIssueSprintReport":
+        """
+        Captures all the relevant fields from the Jira sprint report JSON response.
+
+        Parameters
+        __________
+        key: str
+            The given issue identification key string given by Jira.
+
+        Returns
+        _______
+        JiraIssueSprintReport object
+            A Python object with the selected Jira issue fields within the sprint report
+            API response.
+        """
+        jira_issue_id: int = int(utils.get_object_str(obj, "id"))
+        key: str = utils.get_object_str(obj, "key")
+        summary: str = utils.get_object_str(obj, "key")
+        original_estimate: int = int(
+            utils.get_object_str(obj, "currentEstimateStatistic.statFieldValue.value")
+        )
+        final_estimate: Optional[int] = utils.get_optional_int(
+            obj, "estimateStatistic.statFieldValue.value"
+        )
+        return JiraIssueSprintReport(
+            jira_issue_id, key, summary, original_estimate, final_estimate
+        )
 
     def to_dict(self) -> dict:
+        """
+        Returns the captured fields of the Jira issue within the sprint report rest api
+        in a dict object.
+
+        Returns
+        _______
+        A dict representtation of the JiraIssueSprintReport object.
+        """
+        result: dict = {}
+        result["jira_issue_id"] = int(self.jira_issue_id)
+        result["key"] = str(self.key)
+        result["summary"] = str(self.summary)
+        result["original_estimate"] = int(self.original_estimate)
+        if self.final_estimate is not None:
+            result["final_estimate"] = int(self.final_estimate)
+        return result
+
+
+@dataclass
+class SprintReport:
+    """
+    The SprintReport object captures the most relevant data of the Jira sprint report rest api.
+
+    ...
+    Atributes
+    _________
+    sprint_id: int
+        The sprint id given by Jira.
+
+    """
+
+    ## TODO: Add the rest of the SprintReport fields
+    ## Include issues that were planned originally, removed, and added to a Sprint
+    sprint_id: int
+    name: str
+    goal: str
+    start_date: datetime
+    end_date: datetime
+    completed_issues: list[JiraIssueSprintReport]
+
+    @staticmethod
+    def from_dict(obj: Any) -> "SprintReport":
+        """
+        Captures all the relevant fields from the original JSON resonse
+
+        Parameters
+        _________
+        sprint_id: int
+            The given identification number given by Jira.
+
+        Returns
+        _______
+        SprintReport object
+            A Python object with the selected fields.
+        """
+        sprint_id: int = int(utils.get_object_str(obj, "sprint.id"))
+        name: str = utils.get_object_str(obj, "sprint.name")
+        goal: str = utils.get_object_str(obj, "sprint.goal")
+        start_date: datetime = utils.get_object_datetime_sprint_report(
+            obj, "sprint.activatedDate"
+        )
+        end_date: datetime = utils.get_object_datetime_sprint_report(
+            obj, "sprint.completeDate"
+        )
+        completed_issues: list[JiraIssueSprintReport] = (
+            utils.get_jira_issue_sprint_report_list(obj, "contents.completedIssues")
+        )
+        return SprintReport(
+            sprint_id, name, goal, start_date, end_date, completed_issues
+        )
+
+    def to_dict(self) -> dict:
+        """
+        Returns the captured fields in a dict object.
+
+        Returns
+        _______
+        A dict representation of a SprintReport object.
+        """
         result: dict = {}
         result["sprint_id"] = str(self.sprint_id)
+        result["name"] = self.name
+        result["goal"] = self.goal
+        result["start_date"] = str(self.start_date)
+        result["end_date"] = str(self.end_date)
+        result["completed_issues"] = self.completed_issues
         return result
 
 
 def sprint_report_from_dict(s: Any) -> SprintReport:
+    """
+    Returns a SprintReport object
+
+    Parameters
+    __________
+    s: Any
+        JSON response from the Jira sprint report rest api.
+
+    Returns
+    _______
+    SprintReport object
+        The Python object contains the most relevant fields from the Jira
+        rest api.
+    """
     return SprintReport.from_dict(s)
 
 
 def sprint_report_to_dict(x: SprintReport) -> dict:
+    """
+    Returns a dict object or that can be casted as a JSON.
+
+    Parameters
+    __________
+    x: SprintReport
+        SprintReport object
+
+    Returns
+    _______
+    dict
+        A dict with the data of the SprintReport object.
+    """
     return x.to_dict()
+
+
+def jira_issue_sprint_report_from_dict(s: Any) -> JiraIssueSprintReport:
+    return JiraIssueSprintReport.from_dict(s)
+
+
+def jira_issue_sprint_report_to_dict(s: JiraIssueSprintReport) -> dict:
+    return JiraIssueSprintReport.to_dict(s)
--- utilities/utils.py
+++ utilities/utils.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from typing import Any, Optional
 
+from entities import sprint_report_api
+
 
 def build_get_object_path(object_name: str, name: str) -> str:
     path_list: list[str] = name.split(".")
@@ -45,16 +47,50 @@ def get_object_simple_list(object_name: Any, path: str) -> list[str]:
     return result_list
 
 
+def get_jira_issue_sprint_report_list(
+    object_name: Any, path: str
+) -> list[sprint_report_api.JiraIssueSprintReport]:
+    path_list: list[str] = path.split(".")
+    result: Any = object_name
+    result_list: list[sprint_report_api.JiraIssueSprintReport] = []
+    for element in path_list:
+        result = result.get(element)
+        print(result)
+    for item in result:
+        completed_issue: sprint_report_api.JiraIssueSprintReport = (
+            sprint_report_api.JiraIssueSprintReport.from_dict(item)
+        )
+        result_list.append(completed_issue)
+    return result_list
+
+
 def get_object_datetime(
     object_name: Any, path: str, date_format: str = "%Y-%m-%dT%H:%M:%S"
 ) -> datetime:
     return datetime.strptime(get_object_str(object_name, path)[:-9], date_format)
 
 
+def get_object_datetime_sprint_report(
+    object_name: Any, path: str, date_format: str = "%d/%b/%y %H:%M %p"
+) -> datetime:
+    datetime_str: str = get_object_str(object_name, path)
+    print(datetime_str)
+    if len(datetime_str) < 18:
+        datetime_str = datetime_str[:10] + "1" + datetime_str[10:]
+    return datetime.strptime(get_object_str(object_name, path), date_format)
+
+
 def get_optional_str(object_name: Any, path: str) -> Optional[str]:
     result: Optional[str] = None
     if path in object_name:
         result: Optional[str] = get_object_str(object_name, path)
+    return result
+
+
+def get_optional_int(object_name: Any, path: str) -> Optional[int]:
+    result: Optional[int] = None
+    if path in object_name:
+        result: Optional[int] = int(get_object_str(object_name, path))
     return result
 
 
